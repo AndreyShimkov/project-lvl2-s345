9ec617cc0132f46208e9167b79b37e121f3941bc
Andrey Shimkov <zark2002@mail.ru>
2018-10-08 15:50:24 +0300

    commander fix

--- src/commander/commander.js
+++ src/commander/commander.js
@@ -4,12 +4,12 @@ import { version } from '../../package.json';
 const commander = require('commander');
 
 commander
-  .version(version, '-V, --version')
+  .version(version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<firstconfig>, <secondconfig>')
   .parse(process.argv);
 
-console.log(commander.args);
+// console.log(commander.args); // служебная строка, будет удалена
 
 export default commander;
